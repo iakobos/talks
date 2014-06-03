4f8ecae51222b60ae8fa2f5217d159ad499a94a3
Jake Smith <jake@mustconverge.com>
2014-06-03 13:05:52 -0400

    timecop_3

--- timecop/tests/unit/components/countdown-clock-test.js
+++ timecop/tests/unit/components/countdown-clock-test.js
@@ -5,3 +5,15 @@ moduleForComponent('countdown-clock', 'CountdownClockComponent', {});
 test("it should exist", function(){
   ok(this.subject());
 });
+
+test("it should show YMWDHMS", function(){
+  this.$(); // renders the component
+  var component = this.subject();
+
+  var parts = ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'];
+
+  parts.forEach(function(p){
+    var digit = component.$('.' + p + ' .digits span').text();
+    strictEqual(digit, "00");
+  });
+});
