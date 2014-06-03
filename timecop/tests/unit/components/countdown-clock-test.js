2c8f041068070daf1719c0130d0829aba23b7e7c
Jake Smith <jake@mustconverge.com>
2014-06-03 12:35:21 -0400

    it should exist

---
+++ timecop/tests/unit/components/countdown-clock-test.js
@@ -0,0 +1,7 @@
+import {test, moduleForComponent} from 'ember-qunit';
+
+moduleForComponent('countdown-clock', 'CountdownClockComponent', {});
+
+test("it should exist", function(){
+  ok(this.subject());
+});
